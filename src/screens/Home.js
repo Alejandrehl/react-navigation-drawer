e2ae8b64c46a4aa1a3ac1e8d189c8c9214c976c6
Alejandro Hernández <Alejandrehl@icloud.com>
2019-10-02 18:02:40 -0400

    Toggle done

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, Button } from "react-native";
 
-const Home = () => {
+const Home = ({ navigation }) => {
   return (
     <View style={styles.viewBody}>
       <Text>Home Screen</Text>
+      <Button title="Toggle" onPress={() => navigation.toggleDrawer()} />
     </View>
   );
 };
