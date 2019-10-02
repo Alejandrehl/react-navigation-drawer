c899bf798ddbf678c09f39ca70ebf526d924df83
Alejandro Hernández <Alejandrehl@icloud.com>
2019-10-02 17:30:35 -0400

    Save

---
+++ src/screens/Home.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Text, View, Image, ScrollView, StyleSheet } from "react-native";
+import { Constants } from "expo";
+
+const Home = () => {};
+
+const styles = StyleSheet.create({});
+
+export default Home;
